23ce670c5cf24f491f99081f941b24d69785c78f
Rorii <Rorii@192.168.5.144>
2023-04-09 21:26:53 +0800

    Test

--- ActivitySheet2A/src/activitysheet2a/InheritanceActivityMainGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/InheritanceActivityMainGrp8.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package activitysheet2a;
 
-/**
- *
- * @author Allen James Laxamana
- */
 public class InheritanceActivityMainGrp8 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         MemberClassGrp8 getName = new MemberClassGrp8();
