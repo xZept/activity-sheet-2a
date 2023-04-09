25f7d79dcd8afd87445d4b265c9f1fc4f39da809
hielp <hielp@MSI>
2023-04-09 21:34:47 +0800

    Comment

--- ActivitySheet2A/src/activitysheet2a/InheritanceActivityMainGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/InheritanceActivityMainGrp8.java
@@ -4,9 +4,10 @@ public class InheritanceActivityMainGrp8 {
 
     public static void main(String[] args) {
         
+        //Objects for name and membertype
         MemberClassGrp8 getName = new MemberClassGrp8();
         MemberClassGrp8 getMemType = new MemberClassGrp8();
-            
+        
         System.out.println("Enter Member Name: ");
         String name = getName.getName();
         System.out.println("Enter Member Type: ");
