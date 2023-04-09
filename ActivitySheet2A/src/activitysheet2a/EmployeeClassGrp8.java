9a5d707f4bd3853c8116e5b83e34058fdfa4bac2
User's <User's@DESKTOP-85A9Q0P>
2023-04-09 21:02:25 +0800

    Commit

--- ActivitySheet2A/src/activitysheet2a/EmployeeClassGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/EmployeeClassGrp8.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package activitysheet2a;
 
-/**
- *
- * @author Allen James Laxamana
- */
 public class EmployeeClassGrp8 {
-    
+    public void a(){
+        System.out.println("Shadi");
+}
 }
