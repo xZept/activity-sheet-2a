84b7f6a5be9283f5cc71174dee7cdb40dc8a2e0c
Rorii <Rorii@192.168.5.144>
2023-04-09 21:29:18 +0800

    Remove comments

--- ActivitySheet2A/src/activitysheet2a/MemberClassGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/MemberClassGrp8.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package activitysheet2a;
 
 import java.util.*;
 
-/**
- *
- * @author Allen James Laxamana
- */
 public class MemberClassGrp8 {
     
     public String getName(){
