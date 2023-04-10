80701f9cab4dc1c57b0f6dbeb66967039cf73b7f
Allen James Laxamana <Allen James Laxamana@LAPTOP-MU0VAU8C>
2023-04-10 19:41:06 +0800

    Write the main method along with the other user-defined methods in the main class

--- ActivitySheet2A/src/activitysheet2a/InheritanceActivityMainGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/InheritanceActivityMainGrp8.java
@@ -1,20 +1,44 @@
 package activitysheet2a;
 
+import java.util.Scanner;
+
 public class InheritanceActivityMainGrp8 {
 
-    public static void main(String[] args) {
-        
-        //Objects for name and membertype
-        MemberClassGrp8 getName = new MemberClassGrp8();
-        MemberClassGrp8 getMemType = new MemberClassGrp8();
-        
-        System.out.println("Enter Member Name: ");
-        String name = getName.getName();
-        System.out.println("Enter Member Type: ");
-        String memtype = getMemType.getMemType();
-        
-        
-        
+    static Scanner scan = new Scanner(System.in);
+    static String name, memType;
+    double computedSalary;
+    
+    // Constructor for Greeting message
+    public InheritanceActivityMainGrp8() {
+        System.out.println("Welcome to BPSU Profile!");
+        System.out.println("Hello, " + name);
     }
     
+    public void displayEmpRemarks() {
+        System.out.println("Employee Type: " + memType);
+        System.out.println("Computed Salary: " + computedSalary);
+    }
+
+    public static void main(String[] args) {
+        MemberClassGrp8 obj = new MemberClassGrp8();
+        name = obj.getName();
+        memType = obj.getMemType();
+        System.out.print("\n"); // Whitespace
+        InheritanceActivityMainGrp8 greetUser = new InheritanceActivityMainGrp8();
+        
+        // Check member type
+        switch (memType.toLowerCase()) {
+            case "employee":
+                EmployeeClassGrp8 emp = new EmployeeClassGrp8();
+                System.out.println(emp.computeSalary());
+            case "teaching":
+                
+            case "non-teaching":
+                
+            case "student":
+                
+            default:
+                
+        }
+    }
 }
