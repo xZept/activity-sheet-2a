a4a710ca4dcc2e4f9bde2b903fcb1d4ea650d9b8
Allen James Laxamana <Allen James Laxamana@LAPTOP-MU0VAU8C>
2023-04-10 19:42:27 +0800

    Write the method that will compute the employee's salary

--- ActivitySheet2A/src/activitysheet2a/EmployeeClassGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/EmployeeClassGrp8.java
@@ -1,7 +1,13 @@
 package activitysheet2a;
 
-public class EmployeeClassGrp8 {
-    public void a(){
-        System.out.println("Shadi");
-}
+public class EmployeeClassGrp8 extends MemberClassGrp8 {
+    
+    private int days;
+    
+    // Compute the monthly salary based number of days worked
+    public double computeSalary() {
+        System.out.print("\n" + "Enter number of days worked in a month: ");
+        days = scan.nextInt();
+        return days * 345.50;
+    }
 }
