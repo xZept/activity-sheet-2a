f29f0682523c16d681a74cffab10b5208b4bd5d6
Allen James Laxamana <Allen James Laxamana@LAPTOP-MU0VAU8C>
2023-04-10 20:29:11 +0800

    Write the student class

--- ActivitySheet2A/src/activitysheet2a/StudentClassGrp8.java
+++ ActivitySheet2A/src/activitysheet2a/StudentClassGrp8.java
@@ -1,5 +1,7 @@
 package activitysheet2a;
 
-public class StudentClassGrp8 {
-    
+public class StudentClassGrp8 extends MemberClassGrp8 {
+    public double getUnits(int units) {
+        return units;
+    }
 }
